ea35736dc9a593e2d24bcacf32e69b38ee36fdb7
OliviaSong <2170254948@qq.com>
2018-07-20 20:28:26 +0800

    update wireversion

--- wire/msgversion.go
+++ wire/msgversion.go
@@ -20,7 +20,7 @@ import (
 const MaxUserAgentLen = 256
 
 // DefaultUserAgent for wire in the stack
-const DefaultUserAgent = "/dcrwire:0.3.0/"
+const DefaultUserAgent = "/hcwire:0.3.0/"
 
 // MsgVersion implements the Message interface and represents a decred version
 // message.  It is used for a peer to advertise itself as soon as an outbound
