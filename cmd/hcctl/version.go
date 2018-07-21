f88c8471e643aec6b42550a6fbca6d1d8bcc9d0f
OliviaSong <2170254948@qq.com>
2018-07-21 17:28:31 +0800

    update version

--- cmd/hcctl/version.go
+++ cmd/hcctl/version.go
@@ -18,9 +18,9 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 // These constants define the application version and follow the semantic
 // versioning 2.0.0 spec (http://semver.org/).
 const (
-	appMajor uint = 0
-	appMinor uint = 1
-	appPatch uint = 0
+	appMajor uint = 2
+	appMinor uint = 0
+	appPatch uint = 1
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
