98fbbce99a3f7682bcf643d32978c2a6f9f35041
akash-signh <wxw1198@gmail.com>
2018-09-30 20:08:24 +0800

    modify testnet params

--- wire/protocol.go
+++ wire/protocol.go
@@ -102,7 +102,7 @@ const (
 	RegTest CurrencyNet = 0xc07cb5dd
 
 	// TestNet2 represents the 2nd test network.
-	TestNet2 CurrencyNet = 0x9ab5693b
+	TestNet2 CurrencyNet = 0x9ab5693a
 
 	// SimNet represents the simulation test network.
 	SimNet CurrencyNet = 0x8a136344
