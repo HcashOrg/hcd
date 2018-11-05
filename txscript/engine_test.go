a2ed57c271717225183b56989dc6009e6d632bcb
TriRikkie <a123456>
2018-11-05 15:07:36 +0800

    update txscript

--- txscript/engine_test.go
+++ txscript/engine_test.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2016 The btcsuite developers
-// Copyright (c) 2015-2017 The Decred developers 
+// Copyright (c) 2015-2017 The Decred developers
 // Copyright (c) 2018-2020 The Hc developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
@@ -227,66 +227,6 @@ func TestInvalidFlagCombinations(t *testing.T) {
 	}
 }
 
-// TestCheckPubKeyEncoding ensures the internal checkPubKeyEncoding function
-// works as expected.
-func TestCheckPubKeyEncoding(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name    string
-		key     []byte
-		isValid bool
-	}{
-		{
-			name: "uncompressed ok",
-			key: decodeHex("0411db93e1dcdb8a016b49840f8c53bc1eb68" +
-				"a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf" +
-				"9744464f82e160bfa9b8b64f9d4c03f999b8643f656b" +
-				"412a3"),
-			isValid: true,
-		},
-		{
-			name: "compressed ok",
-			key: decodeHex("02ce0b14fb842b1ba549fdd675c98075f12e9" +
-				"c510f8ef52bd021a9a1f4809d3b4d"),
-			isValid: true,
-		},
-		{
-			name: "compressed ok",
-			key: decodeHex("032689c7c2dab13309fb143e0e8fe39634252" +
-				"1887e976690b6b47f5b2a4b7d448e"),
-			isValid: true,
-		},
-		{
-			name: "hybrid",
-			key: decodeHex("0679be667ef9dcbbac55a06295ce870b07029" +
-				"bfcdb2dce28d959f2815b16f81798483ada7726a3c46" +
-				"55da4fbfc0e1108a8fd17b448a68554199c47d08ffb1" +
-				"0d4b8"),
-			isValid: false,
-		},
-		{
-			name:    "empty",
-			key:     nil,
-			isValid: false,
-		},
-	}
-
-	flags := txscript.ScriptVerifyStrictEncoding
-	for _, test := range tests {
-		err := txscript.TstCheckPubKeyEncoding(test.key, flags)
-		if err != nil && test.isValid {
-			t.Errorf("checkSignatureEncoding test '%s' failed "+
-				"when it should have succeeded: %v", test.name,
-				err)
-		} else if err == nil && !test.isValid {
-			t.Errorf("checkSignatureEncooding test '%s' succeeded "+
-				"when it should have failed", test.name)
-		}
-	}
-
-}
-
 // TestCheckSignatureEncoding ensures the internal checkSignatureEncoding
 // function works as expected.
 func TestCheckSignatureEncoding(t *testing.T) {
@@ -456,4 +396,64 @@ func TestCheckSignatureEncoding(t *testing.T) {
 				"when it should have failed", test.name)
 		}
 	}
+}
+
+// TestCheckPubKeyEncoding ensures the internal checkPubKeyEncoding function
+// works as expected.
+func TestCheckPubKeyEncoding(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		key     []byte
+		isValid bool
+	}{
+		{
+			name: "uncompressed ok",
+			key: decodeHex("0411db93e1dcdb8a016b49840f8c53bc1eb68" +
+				"a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf" +
+				"9744464f82e160bfa9b8b64f9d4c03f999b8643f656b" +
+				"412a3"),
+			isValid: true,
+		},
+		{
+			name: "compressed ok",
+			key: decodeHex("02ce0b14fb842b1ba549fdd675c98075f12e9" +
+				"c510f8ef52bd021a9a1f4809d3b4d"),
+			isValid: true,
+		},
+		{
+			name: "compressed ok",
+			key: decodeHex("032689c7c2dab13309fb143e0e8fe39634252" +
+				"1887e976690b6b47f5b2a4b7d448e"),
+			isValid: true,
+		},
+		{
+			name: "hybrid",
+			key: decodeHex("0679be667ef9dcbbac55a06295ce870b07029" +
+				"bfcdb2dce28d959f2815b16f81798483ada7726a3c46" +
+				"55da4fbfc0e1108a8fd17b448a68554199c47d08ffb1" +
+				"0d4b8"),
+			isValid: false,
+		},
+		{
+			name:    "empty",
+			key:     nil,
+			isValid: false,
+		},
+	}
+
+	flags := txscript.ScriptVerifyStrictEncoding
+	for _, test := range tests {
+		err := txscript.TstCheckPubKeyEncoding(test.key, flags)
+		if err != nil && test.isValid {
+			t.Errorf("checkSignatureEncoding test '%s' failed "+
+				"when it should have succeeded: %v", test.name,
+				err)
+		} else if err == nil && !test.isValid {
+			t.Errorf("checkSignatureEncooding test '%s' succeeded "+
+				"when it should have failed", test.name)
+		}
+	}
+
 }
