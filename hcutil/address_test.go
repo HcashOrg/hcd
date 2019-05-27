333e5e502e8165fd227beb0a5488c4319b40b641
TriRikkie <13641537547@163.com>
2019-05-27 19:13:19 +0800

    Update address_test.go

--- hcutil/address_test.go
+++ hcutil/address_test.go
@@ -577,3 +577,13 @@ func TestAddresses(t *testing.T) {
 		}
 	}
 }
+// testAddressPubKey makes an AddressPubKey, setting the unexported fields with
+// the parameters.
+func testAddressPubKey(serializedPubKey []byte, pubKeyFormat PubKeyFormat, netID [2]byte) *AddressSecpPubKey {
+	pubKey, _ := secp256k1.ParsePubKey(serializedPubKey)
+	return &AddressSecpPubKey{
+		pubKeyFormat: pubKeyFormat,
+		pubKey:       chainec.PublicKey(pubKey),
+		pubKeyHashID: netID,
+	}
+}
