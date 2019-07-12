adfbe65e1acb977071dac854f43cff5adbb46655
GladysKellogg <MaxFabiansAn@yahoo.com>
2019-07-12 14:30:03 +0800

    fix merge err

--- rpcserver.go
+++ rpcserver.go
@@ -5459,11 +5459,6 @@ func handleSendInstantRawTransaction(s *rpcServer, cmd interface{}, closeChan <-
 	c := cmd.(*hcjson.SendInstantRawTransactionCmd)
 	allowHighFees := *c.AllowHighFees
 	hexStr := c.HexTx
-
-	return sendAiRawTransaction(s, hexStr, allowHighFees)
-}
-
-func sendAiRawTransaction(s *rpcServer, hexStr string, allowHighFees bool) (interface{}, error) {
 	if len(hexStr)%2 != 0 {
 		hexStr = "0" + hexStr
 	}
@@ -5497,11 +5492,11 @@ func handleInstantTransaction(s *rpcServer, closeChan <-chan struct{},serialized
 	}
 
 	//check conflict with mempool
-
 	missedParent, err := s.server.blockManager.ProcessInstantTx(instantTx, false, false, allowHighFees)
 	if err != nil || len(missedParent) != 0 {
 		return nil, err
 	}
+
 	instantTxs := make([]*hcutil.InstantTx, 0)
 	instantTxs = append(instantTxs, instantTx)
 
@@ -5516,13 +5511,8 @@ func handleInstantTransaction(s *rpcServer, closeChan <-chan struct{},serialized
 
 }
 
-<<<<<<< .mine
 
 
-=======
-
-
->>>>>>> .theirs
 func handleSendInstantTxVote(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) (interface{}, error) {
 	c := cmd.(*hcjson.SendInstantTxVoteCmd)
 
@@ -5644,11 +5634,11 @@ func handleSendRawTransaction(s *rpcServer, cmd interface{}, closeChan <-chan st
 
 	tx := hcutil.NewTx(msgtx)
 
-	//check instantTx
-	if _, isInstantTx := txscript.IsInstantTx(msgtx); isInstantTx {
-		existAndVoted := s.server.txMemPool.IsInstantTxExistAndVoted(tx.Hash())
-		if !existAndVoted {
-			return sendAiRawTransaction(s, hexStr, allowHighFees)
+
+	//check instant
+	if _,isInstant:=txscript.IsInstantTx(msgtx);isInstant{
+		if !s.server.txMemPool.IsInstantTxExistAndVoted(tx.Hash()){
+			return handleInstantTransaction(s,closeChan,serializedTx,allowHighFees)
 		}
 	}
 
