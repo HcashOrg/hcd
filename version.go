2b7627d2cd400cbd4eba6871ba756a6a0dff1783
robinsonking <878950187@qq.com>
2019-08-07 16:01:07 +0800

    update version to 3.0.1.

--- version.go
+++ version.go
@@ -20,7 +20,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 3
 	appMinor uint = 0
-	appPatch uint = 0
+	appPatch uint = 1
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
