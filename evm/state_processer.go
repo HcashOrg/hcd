911d8f9ccae4f9519b260e8a8c1978fe3117934e
GladysKellogg <MaxFabiansAn@yahoo.com>
2019-11-01 11:44:28 +0800

    add processer

---
+++ evm/state_processer.go
@@ -0,0 +1,59 @@
+package evm
+
+import (
+	"github.com/HcashOrg/hcd/evm/common"
+	"github.com/HcashOrg/hcd/evm/crypto"
+	"github.com/HcashOrg/hcd/evm/params"
+	"github.com/HcashOrg/hcd/evm/state"
+	"github.com/HcashOrg/hcd/evm/types"
+	"github.com/HcashOrg/hcd/evm/vm"
+)
+
+// ApplyTransaction attempts to apply a transaction to the given state database
+// and uses the input parameters for its environment. It returns the receipt
+// for the transaction, gas used and an error if the transaction failed,
+// indicating the block was invalid.
+func ApplyTransaction(config *params.ChainConfig, bc ChainContext, author *common.Address, gp *GasPool,
+	statedb *state.StateDB, header *types.Header, tx *types.Transaction, usedGas *uint64, cfg vm.Config) (*types.Receipt, error) {
+	msg, err := tx.AsMessage()
+	if err != nil {
+		return nil, err
+	}
+	// Create a new context to be used in the EVM environment
+	context := NewEVMContext(msg, header, bc, author)
+	// Create a new environment which holds all relevant information
+	// about the transaction and calling mechanisms.
+	vmenv := vm.NewEVM(context, statedb, config, cfg)
+	// Apply the transaction to the current state (included in the env)
+	_, gas, failed, err := ApplyMessage(vmenv, msg, gp)
+	if err != nil {
+		return nil, err
+	}
+	// Update the state with pending changes
+	var root []byte
+	//if config.IsByzantium(header.Number) {
+	statedb.Finalise(true)
+	//} else {
+	//	root = statedb.IntermediateRoot(config.IsEIP158(header.Number)).Bytes()
+	//}
+	*usedGas += gas
+
+	// Create a new receipt for the transaction, storing the intermediate root and gas used by the tx
+	// based on the eip phase, we're passing whether the root touch-delete accounts.
+	receipt := types.NewReceipt(root, failed, *usedGas)
+	receipt.TxHash = tx.Hash()
+	receipt.GasUsed = gas
+	// if the transaction created a contract, store the creation address in the receipt.
+	if msg.To() == nil {
+		receipt.ContractAddress = crypto.CreateAddress(vmenv.Context.Origin, tx.Nonce())
+	}
+	// Set the receipt logs and create a bloom for filtering
+	receipt.Logs = statedb.GetLogs(tx.Hash())
+	receipt.Bloom = types.CreateBloom(types.Receipts{receipt})
+	receipt.BlockHash = statedb.BlockHash()
+	receipt.BlockNumber = header.Number
+	receipt.TransactionIndex = uint(statedb.TxIndex())
+
+	return receipt, err
+}
+
